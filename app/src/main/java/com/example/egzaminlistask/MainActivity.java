d78540fd0b76afcfb80dc845d8ffc3544d5b6bf7
skpl57 <kania.skpl57@gmail.com>
2026-01-13 08:08:07 +0100

    widok aplikacji

--- app/src/main/java/com/example/egzaminlistask/MainActivity.java
+++ app/src/main/java/com/example/egzaminlistask/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.egzaminlistask;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listaZadan;
+    private TextView opisZadania;
+    private Button dodajBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        listaZadan = findViewById(R.id.lista);
+        opisZadania = findViewById(R.id.opisText);
+        dodajBtn = findViewById(R.id.dodajBtn);
+
+        dodajBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dodawanieDoListy();
+                    }
+                }
+        );
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+            MainActivity.this
+        );
+        listaZadan.setAdapter();
+
+    }
+    private void dodawanieDoListy(){
+
     }
 }
